a6b0a117d7f81742627f78be69a93766276ef662
Oliver Hynds <oliver.hynds@gmail.com>
2017-11-05 11:46:11 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+  'auth' => 'Your-random-string-here',  #this is the authorisation code used for all internal authenticaion -> make it long!
+    
+	'username' => 'user', #This is the username used to log in.
+	'password' => 'pass', #This is the password used to log in.
+);
+?>
