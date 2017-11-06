8f8acac0119f11bda4a99588f4bff5113900894c
BenTechy66 <sendbenspam@yahoo.co.uk>
2017-11-06 17:50:20 +0000

    Create msmverify.php

---
+++ msmverify.php
@@ -0,0 +1,10 @@
+<?php
+if ($_POST["password"] == "password") {
+    $auth_string = "your-random-string-here";
+    setcookie("token", $auth_string); // 86400 = 1 day
+    echo "Auth Token Set, Redirecting...";
+    header('Location: http://ben.mctrees.net/msmcss.html');
+} else {
+    echo "Incorrect password!";   
+}
+?>
