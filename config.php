d40f019e39cf7f8e7beb737faecbf82ebea31e53
BenTechy66 <sendbenspam@yahoo.co.uk>
2017-11-06 19:54:23 +0000

    Delete unnecessary lines

--- config.php
+++ config.php
@@ -2,7 +2,6 @@
 return array(
   'auth' => 'Your-random-string-here',  #this is the authorisation code used for all internal authenticaion -> make it long!
     
-	'username' => 'user', #This is the username used to log in.
 	'password' => 'pass', #This is the password used to log in.
 );
 ?>
